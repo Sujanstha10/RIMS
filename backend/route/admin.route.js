42fce52910066689e8c23bfbd873897614943e03
Sujanstha10 <shresthasujan148@gmail.com>
2023-05-26 09:42:15 +0545

    lansfdgaf

---
+++ backend/route/admin.route.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const router = express.Router();
+const adminController = require("../controller/admin.controller")
+const {validateAdmin} = require("../middleware/FormValidator")
+
+const token = require("../middleware/verifacation")
+
+
+
+
+router.post("/login",validateAdmin,adminController.login)
+router.get("/me",token.verification,adminController.profile)
+
+
+
+
+module.exports=router
