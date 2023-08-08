ec95803a4719b1cb8af1c41d648a7fd045c3dc1d
Sujan Shrestha <sujan@Sujans-MacBook-Air.local>
2023-08-08 13:57:51 +0545

    whatever

--- backend/controller/order.controller.js
+++ backend/controller/order.controller.js
@@ -22,51 +22,79 @@ const model = require("../models");
 
 // ]
 
-const addOrder = (async(req, res) => {
-  try{
-   await model.sequelize.transaction(async (transaction) => {
-
-      console.log(req.body);
-  
-  
-      let customerId = req.body.customerId
-     
+const addOrder = async (req, res) => {
+  try {
+    await model.sequelize.transaction(async (transaction) => {
+      let customerId = req.body.customerId;
       const orderNew = await model.order.create(
-          {
-            customerId,
-          },
-          {
-            transaction,
-          }
-        )
-
+        {
+          customerId,
+        },
+        {
+          transaction,
+        }
+      );
       await Promise.all(
-         req.body.data.map(async(item) => {
-  
+        req.body.data.map(async (item) => {
           const newProdcutOrder = {
             orderId: orderNew.id,
             productId: item.productId,
-            quantity:item.quantity,
-            unitPrice:item.unitPrice
-          }
-         await model.productOrder.create(newProdcutOrder,{transaction});
+            quantity: item.quantity,
+            unitPrice: item.unitPrice,
+          };
+          await model.productOrder.create(newProdcutOrder, { transaction });
         })
       );
     });
-
     return res.status(200).json({
       message: "order created sucessfully",
     });
-
-  }catch(err){
+  } catch (err) {
     res.status(500).json({
       message: err.message,
       err,
     });
   }
-});
+};
 
+const showProductOrder = (req, res) => {
+  model.productOrder
+    .findAll({
+      include: [
+        {
+          model: model.order,
+          include: [
+            {
+              model: model.customer,
+              attributes: ["name"],
+            },
+          ],
+          attributes: { exclude: ["createdAt", "updatedAt", "id"] },
+        },
+        {
+          model: model.products,
+          attributes: { exclude: ["createdAt", "updatedAt", "id"] },
+        },
+      ],
+      attributes: {
+        exclude: ["createdAt", "updatedAt"],
+      },
+    })
+    .then((result) => {
+      res.status(200).json({
+        result,
+      });
+    })
 
-module.exports={
-  addOrder:addOrder
-}
+    .catch((error) => {
+      res.status(500).json({
+        messege: error.message,
+        error,
+      });
+    });
+};
+
+module.exports = {
+  addOrder: addOrder,
+  showProductOrder: showProductOrder,
+};
--- backend/route/order.route.js
+++ backend/route/order.route.js
@@ -3,5 +3,6 @@ const router = express.Router();
 const orderController = require("../controller/order.controller")
 
 router.post("/new",orderController.addOrder)
+router.get("/",orderController.showProductOrder)
 
 module.exports = router;
