dd192f14b6e361bffe51ce492927e9345dfdf608
Sujan Shrestha <sujan@Sujans-MacBook-Air.local>
2023-08-13 19:00:57 +0545

    order-updated

--- backend/controller/order.controller.js
+++ backend/controller/order.controller.js
@@ -1,4 +1,5 @@
 const model = require("../models");
+const order = require("../models/order");
 
 //[
 //     {
@@ -22,10 +23,92 @@ const model = require("../models");
 
 // ]
 
+// const addOrder = async (req, res) => {
+//   try {
+//     await model.sequelize.transaction(async (transaction) => {
+//       let customerId = req.body.customerId;
+//       const orderNew = await model.order.create(
+//         {
+//           customerId,
+//         },
+//         {
+//           transaction,
+//         }
+//       );
+
+//       //       // Create an array to store productIds and quantities
+//       // const productUpdates = [];
+
+//       // // Populate the productUpdates array
+//       // req.body.data.forEach((item) => {
+//       //   productUpdates.push({
+//       //     productId: item.productId,
+//       //     quantity: item.quantity,
+//       //   });
+//       // });
+
+//       // // Update product quantities
+//       // for (const update of productUpdates) {
+//       //   const { productId, quantity } = update;
+//       //   const product = await model.products.findByPk(productId, { transaction });
+
+//       //   if (product) {
+//       //     // Calculate updated quantity
+//       //     const updatedQuantity = Math.max(product.quantity - quantity, 0);
+
+//       //     // Update the product's quantity in the database
+//       //     await product.update({ quantity: updatedQuantity }, { transaction });
+//       //   }
+//       // }
+
+//       // Populate the productUpdates array
+//       req.body.data.forEach(async(item) => {
+//         const { productId, quantity } = item;
+//         const product = await model.products.findByPk(productId, {
+//           transaction,
+//         });
+
+//         if (product) {
+//           // Calculate updated quantity
+//           const updatedQuantity = product.quantity - quantity;
+
+//           // Update the product's quantity in the database
+//           await product.update({ quantity: updatedQuantity }, { transaction });
+//         }
+//       });
+
+//       await Promise.all(
+//         req.body.data.map(async (item) => {
+//           const newProdcutOrder = {
+//             orderId: orderNew.id,
+//             productId: item.productId,
+//             quantity: item.quantity,
+//             unitPrice: item.unitPrice,
+//             total: item.unitPrice * item.quantity,
+//           };
+
+//           await model.productOrder.create(newProdcutOrder, { transaction });
+//         })
+//       );
+//     });
+//     return res.status(200).json({
+//       message: "order created sucessfully",
+//     });
+//   } catch (err) {
+//     res.status(500).json({
+//       message: err.message,
+//       err,
+//     });
+//   }
+// };
+
+
+
 const addOrder = async (req, res) => {
   try {
     await model.sequelize.transaction(async (transaction) => {
       let customerId = req.body.customerId;
+      let orders = req.body.order;
       const orderNew = await model.order.create(
         {
           customerId,
@@ -35,49 +118,20 @@ const addOrder = async (req, res) => {
         }
       );
 
-      //       // Create an array to store productIds and quantities
-      // const productUpdates = [];
+      const outOfStockProducts = [];
 
-      // // Populate the productUpdates array
-      // req.body.data.forEach((item) => {
-      //   productUpdates.push({
-      //     productId: item.productId,
-      //     quantity: item.quantity,
-      //   });
-      // });
+      // Update product quantities
+      for (const item of orders) {
+        const product = await model.products.findByPk(item.productId, { transaction });
 
-      // // Update product quantities
-      // for (const update of productUpdates) {
-      //   const { productId, quantity } = update;
-      //   const product = await model.products.findByPk(productId, { transaction });
-
-      //   if (product) {
-      //     // Calculate updated quantity
-      //     const updatedQuantity = Math.max(product.quantity - quantity, 0);
-
-      //     // Update the product's quantity in the database
-      //     await product.update({ quantity: updatedQuantity }, { transaction });
-      //   }
-      // }
-
-      // Populate the productUpdates array
-      req.body.data.forEach(async(item) => {
-        const { productId, quantity } = item;
-        const product = await model.products.findByPk(productId, {
-          transaction,
-        });
-
-        if (product) {
-          // Calculate updated quantity
-          const updatedQuantity = product.quantity - quantity;
-
-          // Update the product's quantity in the database
-          await product.update({ quantity: updatedQuantity }, { transaction });
-        }
-      });
-
-      await Promise.all(
-        req.body.data.map(async (item) => {
+        if (!product || product.quantity < item.quantity) {
+          outOfStockProducts.push( product.productName );
+        } else {
+                    // Calculate updated quantity
+                    const updatedQuantity = product.quantity - item.quantity;
+          
+                    // Update the product's quantity in the database
+                    await product.update({ quantity: updatedQuantity },{where:{id:product.id}}, { transaction });
           const newProdcutOrder = {
             orderId: orderNew.id,
             productId: item.productId,
@@ -87,11 +141,18 @@ const addOrder = async (req, res) => {
           };
 
           await model.productOrder.create(newProdcutOrder, { transaction });
-        })
-      );
-    });
-    return res.status(200).json({
-      message: "order created sucessfully",
+        }
+      }
+
+      if (outOfStockProducts.length > 0) {
+        return res.status(200).json({
+          message: 'Out of stock: ' + outOfStockProducts,
+        });
+      } else {
+        return res.status(200).json({
+          message: 'Order created successfully.',
+        });
+      }
     });
   } catch (err) {
     res.status(500).json({
